e46062765eabc59e7e8bfc01e6d3aa168ef4a3a2
l1nf3ng <l1nf3ng@github.com>
2023-12-26 16:10:54 +0800

    增加网络服务探测，判断是否可直接访问。

---
+++ detect/crawler.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+DEFAULT_TIME_OUT=5
+DETECT_HEADER = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0"}
+
+def GetPageTitle(url):
+    try:
+        resp = requests.get(url, headers=DETECT_HEADER, verify=False, timeout=DEFAULT_TIME_OUT)
+        code = resp.status_code
+        html = resp.content
+        tree = BeautifulSoup(html, "html.parser")
+        title = tree.title.contents[0]
+        return code, title
+    except Exception as e:
+        return 600, "TimeOut"
+
+
+if __name__ == "__main__":
+    GetPageTitle("https://www.huolala.cn")
